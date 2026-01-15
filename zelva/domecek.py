840e4399127910ee11dca941d7ada52fb79c5a31
Filip Dostal <filip.dostal.s@gyarab.cz>
2026-01-15 22:46:30 +0100

    Added domecek.py

--- zelva/domecek.py
+++ zelva/domecek.py
@@ -3,20 +3,26 @@ from random import randint
 from turtle import forward, left, right, exitonclick
 
 def domecek(a):
-    c = sqrt(2) * 2
+    c = sqrt(2 * a**2)
+    left(90)
+    forward(a)
+    right(90)
+    forward(a)
+    right(135)
+    forward(c)
+    left(135)
     forward(a)
     left(90)
     forward(a)
+    left(45)
+    forward(c/2)
     left(90)
-    forward(a)
+    forward(c/2)
     left(90)
-    forward(a)
-    left(90)
-    forward(a)
-
-for i in range(10):
+    forward(c)
+    
+for i in range(5):
     domecek(randint(30, 50))
     right(36)
-
 
 exitonclick()
