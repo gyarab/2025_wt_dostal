05b745710ddc59049e0700688235378b2dcfc139
school <filip.dostal.s@p1-7.ad.gyarab.cz>
2026-01-23 12:10:58 +0100

    Improved exchange.py

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,7 +1,8 @@
 import httpx
 
-r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
-print(r.text)
-print(r.status)
-lines = r.text.split('\n')
-print(lines[0])
+res = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
+
+print("Server odpovedel", res.status_code)
+
+lines = res.text.split('\n')
+print("Kurzy pro den:", lines[0].split(' ')[0])
